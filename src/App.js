e3cebf69e0a086952bc3b281d2d4830ac9eacb5c
Kirti Wadhwani <kwadhwani@36eighttechnologies.com>
2024-12-09 18:42:26 -0500

    Changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import About from "./components/about";
